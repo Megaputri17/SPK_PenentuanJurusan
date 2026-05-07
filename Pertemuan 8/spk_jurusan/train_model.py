f4d5bc8f1716ce5f3f409e4e24837616cfd1796b
Megaputri <167510295+Megaputri17@users.noreply.github.com>
2026-05-07 22:42:51 +0700

    Update train_model.py

--- Pertemuan 8/spk_jurusan/train_model.py
+++ Pertemuan 8/spk_jurusan/train_model.py
@@ -1,46 +1,67 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
 import joblib
 
-# 1. Load dataset
-data = pd.read_csv("../ml_api/Dataset_Jurusan.csv")
+# ======================
+# LOAD DATASET
+# ======================
+data = pd.read_csv("Dataset_Jurusan.csv")
 
-# 2. Pisahkan fitur (X) dan label (y)
-# X = nilai siswa
-# y = rekomendasi (0/1)
-X = data[['nilai_akademik', 'minat']]
-y = data['label']
+# ======================
+# FITUR
+# ======================
+X = data[['kompetensi', 'nilai_akademik']]
 
-# 3. Scaling fitur
+# ======================
+# TARGET
+# ======================
+y = data['jurusan_1']
+
+# ======================
+# SCALING
+# ======================
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
-# 4. Split data
+# ======================
+# SPLIT DATA
+# ======================
 X_train, X_test, y_train, y_test = train_test_split(
-    X_scaled, y, test_size=0.2, random_state=42
+    X_scaled,
+    y,
+    test_size=0.2,
+    random_state=42
 )
 
-# 5. Training model Random Forest
-model = RandomForestClassifier(n_estimators=100, random_state=42)
+# ======================
+# MODEL RANDOM FOREST
+# ======================
+model = RandomForestClassifier(
+    n_estimators=100,
+    random_state=42
+)
+
+# ======================
+# TRAINING
+# ======================
 model.fit(X_train, y_train)
 
-# 6. Evaluasi
+# ======================
+# EVALUASI
+# ======================
 y_pred = model.predict(X_test)
+
 akurasi = accuracy_score(y_test, y_pred)
 
-print(f"Akurasi Model: {akurasi * 100:.1f}%")
-print(classification_report(y_test, y_pred))
+print(f"Akurasi Model: {akurasi*100:.1f}%")
 
-# 7. Feature importance
-importances = model.feature_importances_
-print(f"Nilai Akademik importance: {importances[0]:.3f}")
-print(f"Minat importance:          {importances[1]:.3f}")
-
-# 8. Simpan model
-joblib.dump(model,  'model_jurusan.pkl')
+# ======================
+# SIMPAN MODEL
+# ======================
+joblib.dump(model, 'model_jurusan.pkl')
 joblib.dump(scaler, 'scaler_jurusan.pkl')
 
-print("Model dan scaler berhasil disimpan.")
+print("Model berhasil disimpan!")
